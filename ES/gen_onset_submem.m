01df7989aa95cd403cd6e3e4ed1f40efc6b98803
QQXiao <xiaoxq@mail.bnu.edu.cn>
2016-02-29 22:23:31 +0800

    modified gen_onset_submem.m

--- ES/gen_onset_submem.m
+++ ES/gen_onset_submem.m
@@ -1,6 +1,6 @@
 %%%combine behav results from different runs and different stages
 Mtrial=1; % trial number
-MID=2;  
+MID=2;
 PID=3;  % material id
 Mcons=4; %1:CONSISTENT, 2 INCONSISTENT;3: filler/once
 Mfile=5; %picture is from which file
@@ -38,7 +38,7 @@ for sub=1:size(subs,2)
      for runid=1:4
         filename=ls(sprintf('ev_sub%d_run%d*', subid, runid)); %load([runtimelist(runid).name]);
         eval(sprintf('load %s',filename));
-        SM(:,Mrun)=runid;  %add Mrun  
+        SM(:,Mrun)=runid;  %add Mrun
         subln=[subln; SM];        %merge matrix sub and SM
 
 	%% erro trial
@@ -48,46 +48,46 @@ for sub=1:size(subs,2)
         %eval(sprintf('save %s tmp2 -tabs -ascii',outputfile));
     end
     subln=sortrows(subln,[3,12]); % sort according the trial number and study order
-    
+
     % group into 5 condition
    subln(subln(:,4)==1 & subln(:,6)<4,Mcond)=1;% massed consistent
    subln(subln(:,4)==2 & subln(:,6)<4,Mcond)=2;% massed inconsistent
    subln(subln(:,4)==1 & subln(:,6)>4,Mcond)=3;% spaced consistent
    subln(subln(:,4)==2 & subln(:,6)>4,Mcond)=4;% spaced inconsistent
    subln(subln(:,4)==3 & subln(:,6)==520,Mcond)=5;% once
-    
+
     %load word&pic recognization result
     subw=[];
-    for i=1:2 
+    for i=1:2
         filename=ls(sprintf('memtest_words_sub%d_run%d*',subid,i));
         eval(sprintf('load %s',filename));
         SM(:,9)=i; % add run
         subw=[subw;SM];
     end
-    
+
     subw=sortrows(subw,3);
-    
+
     subp=[];
-    for i=1:2 
+    for i=1:2
         filename=ls(sprintf('memtest_pic_sub%d_run%d*',subid,i));
         eval(sprintf('load %s',filename));
         SM(:,12)=i; % add run
         subp=[subp;SM];
     end
     subp=sortrows(subp,3);
-       
+
    % merge learn and test_words data
    for i=1:size(subln,1)
     ix=find(subw(:,3)==subln(i,3));
     subln(i,MWM)=subw(ix,5);
-    
+
     ix=find(subp(:,3)==subln(i,3) & subp(:,6)==subln(i,5)); % find the corresponding pic
     subln(i,MPM)=subp(ix,7);
    end
 
-   
+
    % creat onset file
-   
+
    cond_name={'MC','MI','SC','SI','Once'};
    for run=1:4
         for cond = 1:4
@@ -95,43 +95,43 @@ for sub=1:size(subs,2)
 	    tmp=sortrows(tmp,[3,12]); % sort according the trial number and study order
             for rep = 1:2
                 aa=tmp(rep:2:end,MAonset);
-    		aa=sortrows(aa); 
+    		aa=sortrows(aa);
                 tmp1=[[0 0 0];[aa ones(size(aa)) ones(size(aa))]];
                 eval(sprintf('save %s/sub%03d/behav/Cate_run%d_%s%d.txt -ascii -tabs tmp1',basedir,subs(sub),run,cond_name{cond},rep));
             end
         end
-        
-          aa=subln(subln(:,Mrun)==run & subln(:,Mcond)==5,MAonset);
-    	  aa=sortrows(aa); 
-          tmp1=[[0 0 0];[aa ones(size(aa)) ones(size(aa))]];
-          eval(sprintf('save %s/sub%03d/behav/Cate_run%d_%s.txt -ascii -tabs tmp1',basedir,subs(sub),run,cond_name{5}));  
-          
-          
-          aa=subln(subln(:,Mrun)==run & subln(:,Mcond)==5,MAonset);
-          tmp1=[[0 0 0];[aa ones(size(aa)) ones(size(aa))]];
-          eval(sprintf('save %s/sub%03d/behav/Submem_run%d_filler.txt -ascii -tabs tmp1',basedir,subs(sub),run));  
 
-          aa=subln(subln(:,Mrun)==run & subln(:,Mcond)<5 & subln(:,MWM)==5,MAonset);
+          aa=subln(subln(:,Mrun)==run & subln(:,Mcond)==5,MAonset);
+    	  aa=sortrows(aa);
+          tmp1=[[0 0 0];[aa ones(size(aa)) ones(size(aa))]];
+          eval(sprintf('save %s/sub%03d/behav/Cate_run%d_%s.txt -ascii -tabs tmp1',basedir,subs(sub),run,cond_name{5}));
+
+
+          aa=subln(subln(:,Mrun)==run & subln(:,Mcond)==5,MAonset);
+          tmp1=[[0 0 0];[aa ones(size(aa)) ones(size(aa))]];
+          eval(sprintf('save %s/sub%03d/behav/Submem_run%d_filler.txt -ascii -tabs tmp1',basedir,subs(sub),run));
+
+          aa=subln(subln(:,Mrun)==run & subln(:,Mcond)<5 & subln(:,MWM)<=4,MAonset);
           tmp1=[[0 0 0];[aa(1:2:end) ones(length(aa)/2,1) ones(length(aa)/2,1)]];
-          eval(sprintf('save %s/sub%03d/behav/Submem_run%d_R1.txt -ascii -tabs tmp1',basedir,subs(sub),run));  
-          
+          eval(sprintf('save %s/sub%03d/behav/Submem_run%d_R1.txt -ascii -tabs tmp1',basedir,subs(sub),run));
+
         tmp1=[[0 0 0];[aa(2:2:end) ones(length(aa)/2,1) ones(length(aa)/2,1)]];
-           eval(sprintf('save %s/sub%03d/behav/Submem_run%d_R2.txt -ascii -tabs tmp1',basedir,subs(sub),run));  
-          
-           
-          aa=subln(subln(:,Mrun)==run & subln(:,Mcond)<5 & subln(:,MWM)<5 & subln(:,MWM)>2,MAonset);
+           eval(sprintf('save %s/sub%03d/behav/Submem_run%d_R2.txt -ascii -tabs tmp1',basedir,subs(sub),run));
+
+
+          aa=subln(subln(:,Mrun)==run & subln(:,Mcond)<5 & subln(:,MWM)==3,MAonset);
          tmp1=[[0 0 0];[aa(1:2:end) ones(length(aa)/2,1) ones(length(aa)/2,1)]];
-           eval(sprintf('save %s/sub%03d/behav/Submem_run%d_K1.txt -ascii -tabs tmp1',basedir,subs(sub),run));  
-          
+           eval(sprintf('save %s/sub%03d/behav/Submem_run%d_K1.txt -ascii -tabs tmp1',basedir,subs(sub),run));
+
           tmp1=[[0 0 0];[aa(2:2:end) ones(length(aa)/2,1) ones(length(aa)/2,1)]];
-         eval(sprintf('save %s/sub%03d/behav/Submem_run%d_K2.txt -ascii -tabs tmp1',basedir,subs(sub),run)); 
-          
+         eval(sprintf('save %s/sub%03d/behav/Submem_run%d_K2.txt -ascii -tabs tmp1',basedir,subs(sub),run));
+
           aa=subln(subln(:,Mrun)==run & subln(:,Mcond)<5 & subln(:,MWM)<3,MAonset);
         tmp1=[[0 0 0];[aa(1:2:end) ones(length(aa)/2,1) ones(length(aa)/2,1)]];
-           eval(sprintf('save %s/sub%03d/behav/Submem_run%d_F1.txt -ascii -tabs tmp1',basedir,subs(sub),run));  
-          
+           eval(sprintf('save %s/sub%03d/behav/Submem_run%d_F1.txt -ascii -tabs tmp1',basedir,subs(sub),run));
+
          tmp1=[[0 0 0];[aa(2:2:end) ones(length(aa)/2,1) ones(length(aa)/2,1)]];
-          eval(sprintf('save %s/sub%03d/behav/Submem_run%d_F2.txt -ascii -tabs tmp1',basedir,subs(sub),run));  
+          eval(sprintf('save %s/sub%03d/behav/Submem_run%d_F2.txt -ascii -tabs tmp1',basedir,subs(sub),run));
 
 	%single trial
 	%SM=subln(subln(:,Mrun)==run,:);
